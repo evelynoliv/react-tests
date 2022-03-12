3cad83518fde7ed06a6971a2d2a7da27a8d3b686
Evelyn O <evelynolives@gmail.com>
2022-03-12 19:41:01 -0300

    estrutura

---
+++ calculator/src/Components/Contador/style.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const MainDiv = styled.div `
+background-color: black;
+align-itens: center;
+  
+`
+
+export const KeyPad = styled.div`
+
+button{
+width: 3em;
+height: 3em;
+font-size: 1.5em;
+border: none;
+border-radius: 2em;
+margin:0.3em;
+cursor: pointer;
+:hover {
+  background-color: #EBBE9B; /* Green */
+  color: white;
+}
+
+}
+
+ 
+`;
+
+
+
