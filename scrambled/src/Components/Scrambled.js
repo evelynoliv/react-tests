224bbf2f2f9b460ffe70cfbac3cb9f3b5628daed
Evelyn O <evelynolives@gmail.com>
2022-04-20 18:58:24 -0300

    initial

---
+++ scrambled/src/Components/Scrambled.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+import TextScrambler from 'react-scramble-text'
+import 'react-scramble-text/dist/index.css'
+
+const Example  {
+  return 
+    
+        const phrases = ["The way to get started is to", "quit talking", "and", "begin doing", "-Walt Disney"]
+        return <TextScrambler phrases={phrases}
+                              speed={50}
+                              pauseTime={800}/>
+      
+
+
+
+
+   
+}
+
+export default Example;
